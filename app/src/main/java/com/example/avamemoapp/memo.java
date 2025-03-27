05c1c4e13c0033ebd776ce3ad4a3f5450955baa3
aj-gibbs <alphonzagibbs2@icloud.com>
2025-03-26 22:42:53 -0400

    aj new computer commit

--- app/src/main/java/com/example/avamemoapp/memo.java
+++ app/src/main/java/com/example/avamemoapp/memo.java
@@ -1,4 +1,62 @@
 package com.example.avamemoapp;
 
+import java.util.Calendar;
+
 public class memo {
+
+    private int memoID;
+    private String name;
+
+    private String text;
+
+    private String priority;
+
+    private Calendar date;
+
+    public memo (){
+        memoID = -1;
+        date = Calendar.getInstance();
+    }
+
+    public int getMemoID (){
+        return memoID;
+    }
+
+    public void setMemoID (int memoID){
+        this.memoID = memoID;
+    }
+
+    public String getName (){
+        return name;
+    }
+
+    public void setName (String name){
+        this.name = name;
+    }
+
+    public String getText (){
+        return text;
+    }
+
+    public void setText (String text){
+        this.text = text;
+    }
+
+    public String getPriority (){
+        return priority;
+    }
+
+    public void setPriority (String priority){
+        this.priority = priority;
+    }
+
+    public Calendar getDate (){
+        return date;
+    }
+
+    public void setDate (Calendar date){
+        this.date = date;
+    }
+
+
 }
