5d71c5de7e9bcb2557ec701a840a4b2aecc050ed
aj-gibbs <alphonzagibbs2@icloud.com>
2025-03-31 21:02:59 -0400

    aj new computer commit

---
+++ app/src/main/java/com/example/avamemoapp/workSplit.java
@@ -0,0 +1,38 @@
+package com.example.avamemoapp;
+
+public class workSplit {
+}
+
+/*
+AVA MEMO APP
+
+Members: AJ, Drew, Val
+
+AJ: Main Activity, MemoDataSource, MemoDBHelper, Memo, DatePickerDialog
+- database creation
+- save memo to database
+- load memo from database
+- Validation Message
+- edit memo(on memolist page)
+- date selection
+-
+
+Drew: MemoSettingsActivity
+- search memos
+- display memos based on priority
+- listView
+- filter memos
+- shared preferences
+-
+
+Val: MemoListActivity, MemoAdapter,
+- RecyclerView
+- populating the memo list from database
+- delete memo
+- UI design
+- navigation buttons
+
+
+
+
+*/
